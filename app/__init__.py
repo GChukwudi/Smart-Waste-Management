aefa9e074daa4dc8d5388fc78a851d11326792a1
NadiaTeta <144005791+NadiaTeta@users.noreply.github.com>
2024-06-17 16:14:33 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-from config import Config
+from app.config import Config
 from app import auth, main
 
 db = SQLAlchemy()
