3b21ecebfc0ac0f709c61c4eeae3cec4a4ddeaa9
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-06-19 21:29:37 +0200

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,51 +1,20 @@
-# #!/usr/bin/python3
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from flask_login import LoginManager
-# from flask_wtf.csrf import CSRFProtect
-# from app.config import Config
-
-# db = None
-
-# migrate = Migrate()
-# login = LoginManager()
-# login.login_view = 'auth.login'
-# csrf = CSRFProtect()
-
-
-# def create_app():
-#     global db
-
-#     app = Flask(__name__)
-#     app.config.from_object(Config)
-
-#     from app.routes import bp as main_bp
-
-#     db = SQLAlchemy()
-#     migrate.init_app(app, db)
-#     login.init_app(app)
-#     csrf.init_app(app)
-
-#     app.register_blueprint(main_bp)
-
-#     return app
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcyrpt import Bcrypt
 
 db = SQLAlchemy()
-login = LoginManager()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
-    login.init_app(app)
+    login_manager.init_app(app)
 
     # Import blueprints and register them
     from .routes import bp as main_bp
